e3de4ab9be1d6a8a0b214e40ba0bf949cf958ec7
jamespreed <jamesreed.physics@gmail.com>
2018-11-14 14:50:11 -0500

    Update conda-notes.py

--- conda-notes.py
+++ conda-notes.py
@@ -4,3 +4,5 @@ from conda.cli import conda_argparse
 p = conda_argparse.generate_parser()
 args = p.parse_args(['search','pandas=0.22=py36*','--info'])
 main_search.execute(args, None)
+
+# need to copy-paste conda/cli/main_search.py::execute() to return a result...
