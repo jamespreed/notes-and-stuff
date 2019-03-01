0a2cb8d95bb9b77e8e7fb3e747e4465545e7a88d
James <jamesreed.physics@gmail.com>
2019-02-28 22:28:43 -0500

    Create threadedrmdir.py

---
+++ bottle_conda/threadedrmdir.py
@@ -0,0 +1,34 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from multiprocessing import cpu_count
+from glob import glob
+import shutil
+
+
+class ThreadedFileRemover:
+    def __init__(self, *paths, num_threads=None):
+        """
+        Uses threaded operations to delete folders and files listed as
+        glob `paths`.  Number of threads defaults to 2 x cpu_count 
+        when `num_threads` is 0 or None.
+        """
+        self.paths = []
+        for p in paths:
+            self.paths.extend(glob(p))
+        self.num_threads = num_threads if num_threads else 2*cpu_count()
+        
+    @staticmethod
+    def _rmtree(d):
+        shutil.rmtree(d)
+        return d
+
+    def delete_paths(self):
+        with ThreadPoolExecutor(self.num_threads) as pool:
+            threads = [pool.submit(rmtree, d) for d in dirs]
+            for f in as_completed(threads):
+                try:
+                    d = f.result()
+                except Exception as e:
+                    print('Error occured: ', e)
+                else:
+                    print(f'REMOVED: {d}')
+        return True
